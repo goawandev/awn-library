2b42f18185b458102f6411ecfeeb440b3e12756e
goawandev <goawandev@gmail.com>
2025-10-11 22:45:02 +0700

    refactor: convert all code to ES6 class

--- main.js
+++ main.js
@@ -1,116 +1,148 @@
-const myLibrary = [];
+// const myLibrary = [];
 
-function Book(title, author, pages, isRead) {
-  this.id = crypto.randomUUID();
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
-}
+// function Book(title, author, pages, isRead) {
+//   this.id = crypto.randomUUID();
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.isRead = isRead;
+// }
 
-Book.prototype.toggleReadStatus = function () {
-  this.isRead = !this.isRead;
-};
+// Book.prototype.toggleReadStatus = function () {
+//   this.isRead = !this.isRead;
+// };
 
-// Add book
-function addBookToLibrary(title, author, pages, isRead) {
-  const newBook = new Book(title, author, pages, isRead);
-  myLibrary.push(newBook);
-  displayBook();
-}
+// // Add book
+// function addBookToLibrary(title, author, pages, isRead) {
+//   const newBook = new Book(title, author, pages, isRead);
+//   myLibrary.push(newBook);
+//   displayBook();
+// }
 
-// Display book
-function displayBook() {
-  console.log(myLibrary);
-  const libraryContainer = document.querySelector("#library");
-  libraryContainer.innerHTML = "";
+// // Display book
+// function displayBook() {
+//   console.log(myLibrary);
+//   const libraryContainer = document.querySelector("#library");
+//   libraryContainer.innerHTML = "";
 
-  myLibrary.forEach((book) => {
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("book-card");
-    bookCard.setAttribute("data-id", book.id);
+//   myLibrary.forEach((book) => {
+//     const bookCard = document.createElement("div");
+//     bookCard.classList.add("book-card");
+//     bookCard.setAttribute("data-id", book.id);
 
-    bookCard.innerHTML = `
-    <h3>${book.title}</h3>
-    <p>Author: ${book.author}</p>
-    <p>Pages: ${book.pages}</p>
-    <label>
-    <input type="checkbox" class="read-toggle" ${book.isRead ? "checked" : ""}>
-    Read
-    </label>
-    <button class="remove-btn">Remove</button>
-    `;
+//     bookCard.innerHTML = `
+//     <h3>${book.title}</h3>
+//     <p>Author: ${book.author}</p>
+//     <p>Pages: ${book.pages}</p>
+//     <label>
+//     <input type="checkbox" class="read-toggle" ${book.isRead ? "checked" : ""}>
+//     Read
+//     </label>
+//     <button class="remove-btn">Remove</button>
+//     `;
 
-    libraryContainer.appendChild(bookCard);
-  });
+//     libraryContainer.appendChild(bookCard);
+//   });
 
-  //   EventListener remove book
-  const removeButtons = document.querySelectorAll(".remove-btn");
-  removeButtons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      const bookCard = e.target.closest(".book-card");
-      const bookId = bookCard.getAttribute("data-id");
-      removeBook(bookId);
-    });
-  });
+//   //   EventListener remove book
+//   const removeButtons = document.querySelectorAll(".remove-btn");
+//   removeButtons.forEach((button) => {
+//     button.addEventListener("click", (e) => {
+//       const bookCard = e.target.closest(".book-card");
+//       const bookId = bookCard.getAttribute("data-id");
+//       removeBook(bookId);
+//     });
+//   });
 
-  //   EventListener status read
-  const readToggles = document.querySelectorAll(".read-toggle");
-  readToggles.forEach((checkbox) => {
-    checkbox.addEventListener("change", (e) => {
-      const bookCard = e.target.closest(".book-card");
-      const bookId = bookCard.getAttribute("data-id");
-      toggleBookReadStatus(bookId);
-    });
-  });
-}
+//   //   EventListener status read
+//   const readToggles = document.querySelectorAll(".read-toggle");
+//   readToggles.forEach((checkbox) => {
+//     checkbox.addEventListener("change", (e) => {
+//       const bookCard = e.target.closest(".book-card");
+//       const bookId = bookCard.getAttribute("data-id");
+//       toggleBookReadStatus(bookId);
+//     });
+//   });
+// }
 
-// New book form
-const newBookBtn = document.querySelector("#newBookBtn");
-const bookForm = document.querySelector("#bookForm");
+// // New book form
+// const newBookBtn = document.querySelector("#newBookBtn");
+// const bookForm = document.querySelector("#bookForm");
 
-newBookBtn.addEventListener("click", () => {
-  if (bookForm.style.display === "none" || bookForm.style.display === "") {
-    bookForm.style.display = "flex";
-  } else {
-    bookForm.style.display = "none";
+// newBookBtn.addEventListener("click", () => {
+//   if (bookForm.style.display === "none" || bookForm.style.display === "") {
+//     bookForm.style.display = "flex";
+//   } else {
+//     bookForm.style.display = "none";
+//   }
+// });
+
+// bookForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   const title = document.querySelector("#title").value;
+//   const author = document.querySelector("#author").value;
+//   const pages = document.querySelector("#pages").value;
+//   const isRead = document.querySelector("#isRead").checked;
+
+//   addBookToLibrary(title, author, pages, isRead);
+
+//   bookForm.reset();
+//   bookForm.style.display = "none";
+// });
+
+// // Remove book
+// function removeBook(bookId) {
+//   const index = myLibrary.findIndex((book) => book.id === bookId);
+
+//   if (index !== -1) {
+//     myLibrary.splice(index, 1);
+//     console.log("Buku dihapus. Sisa buku:", myLibrary);
+//     displayBook();
+//   }
+// }
+
+// // Read status function
+// function toggleBookReadStatus(bookId) {
+//   const book = myLibrary.find((b) => b.id === bookId);
+//   if (book) {
+//     book.toggleReadStatus();
+//     displayBook();
+//   }
+// }
+
+// // testing
+// addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false);
+// addBookToLibrary("Clean Code", "Robert C. Martin", 464, true);
+
+// Refactor To Class
+// Book Class
+class Book {
+  constructor(title, author, pages, isRead) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
   }
-});
 
-bookForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
-  const pages = document.querySelector("#pages").value;
-  const isRead = document.querySelector("#isRead").checked;
-
-  addBookToLibrary(title, author, pages, isRead);
-
-  bookForm.reset();
-  bookForm.style.display = "none";
-});
-
-// Remove book
-function removeBook(bookId) {
-  const index = myLibrary.findIndex((book) => book.id === bookId);
-
-  if (index !== -1) {
-    myLibrary.splice(index, 1);
-    console.log("Buku dihapus. Sisa buku:", myLibrary);
-    displayBook();
+  toggleReadStatus() {
+    this.isRead = !this.isRead;
   }
 }
 
-// Read status function
-function toggleBookReadStatus(bookId) {
-  const book = myLibrary.find((b) => b.id === bookId);
-  if (book) {
-    book.toggleReadStatus();
-    displayBook();
+// Library Class
+class Library {
+  constructor() {
+    this.books = [];
+    this.libraryContainer = document.querySelector("#library");
+    this.bookForm = document.querySelector("#bookForm");
+    this.newBookBtn = document.querySelector("#newBookBtn");
+
+    this.setUpEventListener();
+  }
+
+  setUpEventListener() {
+    this.newBookBtn.addEventListener;
   }
 }
-
-// testing
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false);
-addBookToLibrary("Clean Code", "Robert C. Martin", 464, true);
